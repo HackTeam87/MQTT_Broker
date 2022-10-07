24ecbf4bc16e9af443f95069b46ad72c5233c77e
HackTeam87 <asusgrin@gmail.com>
2022-10-07 15:15:38 +0300

    1.2

--- Broker.py
+++ Broker.py
@@ -3,14 +3,20 @@ from geopy import distance
 
 #Локація Університету Телекомунікацій
 BASE_LOC = (50.42887501136511, 30.47598202502631)
+TOPIC = "heat"
 
 def CALCULATE_LOC(INPUT_LOC):
     My_DISTANCE = round(distance.distance(BASE_LOC, INPUT_LOC).meters)
     
     if My_DISTANCE > 500:
-        return(f'Ви знаходитесь за {My_DISTANCE} метрів від Університету Power OFF')
+        return('OFF')
     else:
-        return(f'Ви знаходитесь за {My_DISTANCE} метрів від Університету Power ON')
+        return('ON')
+
+    # if My_DISTANCE > 500:
+    #     return(f'Ви знаходитесь за {My_DISTANCE} метрів від Університету Power OFF')
+    # else:
+    #     return(f'Ви знаходитесь за {My_DISTANCE} метрів від Університету Power ON')    
     
 
 
@@ -20,7 +26,7 @@ def ON_MESSAGE(client, userdata, message):
 
 try:
     print('\nBroker Start')
-    subscribe.callback(ON_MESSAGE, 'userloc' , hostname="localhost")
+    subscribe.callback(ON_MESSAGE, TOPIC , hostname="localhost")
 except:
     print('\nBroker Stop')
     pass    
